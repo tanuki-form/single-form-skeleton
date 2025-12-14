da7fc701e0403f016f0100f80713248d352f3d01
green spot <in.green.spot@gmail.com>
2025-12-14 18:20:31 +0900

    fix

--- config/schema.php
+++ config/schema.php
@@ -6,21 +6,18 @@ return [
   ["name" => "cf-turnstile-response"],
   [
     "name" => "name",
-    "label" => "Name",
     "validation" => [
       "required" => true,
       "minLength" => 2
     ]
   ], [
     "name" => "email",
-    "label" => "Email Address",
     "validation" => [
       "required" => true,
       "email" => true
     ]
   ], [
     "name" => "email2",
-    "label" => "Email Address (confirmation)",
     "validation" => [
       "required" => true,
       "email" => true,
@@ -29,19 +26,16 @@ return [
   ], [
     "type" => "array",
     "name" => "enquete",
-    "label" => "Enquete",
     "validation" => [
       "required" => true
     ]
   ], [
     "name" => "message",
-    "label" => "Message",
     "validation" => [
       "required" => true
     ]
   ], [
     "name" => "agreement",
-    "label" => "Agreement",
     "validation" => [
       "required" => true
     ]
